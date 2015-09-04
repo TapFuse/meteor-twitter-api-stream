8e89217e8d498cd173a6cc99d75de94a1f7f19ee
Merkurijus <matasjonys@gmail.com>
2015-09-04 17:17:57 +0700

    Stripping tweet object

--- twitter-api-streaming.js
+++ twitter-api-streaming.js
@@ -7,7 +7,10 @@ Meteor.startup(function () {
   TweetQueryCache.find().observeChanges({
 		added: function(id, doc) {
 			if (!twtQueries[doc.track]) {
-				Meteor.call('streamStatuses', doc);
+				Meteor.call('streamStatuses', {
+					_id: id,
+					track: doc.track
+				});
 			}
 		},
 		removed: function(id, doc) {
@@ -24,33 +27,44 @@ function twitterCredentials (meteorUser) {
 		return new TwitterApi({
 				consumer_key: config.consumerKey,
 				consumer_secret: config.secret,
-				access_token_key: '1736677940-e8N8Q3va5ZlWby2GSvEEQDVngnDcuAPF7sgouRY',
-				access_token_secret: 'Ny8YptHsTQ87vWUxOk4onZJtMALKLE9JvSqkHo1CHezJl'
+				access_token_key: '',
+				access_token_secret: ''
 		});
 	}
 
 //Insert used to cache tweets from stream.
-var wrappedTweetInsert = Meteor.bindEnvironment(function(tweet) {
-    TweetCache.insert(tweet);
+var wrappedTweetInsert = Meteor.bindEnvironment(function(tweet, track) {
+    TweetCache.insert({
+    	_id: tweet.id_str,
+    	query_id: track,
+    	created_at: tweet.created_at,
+    	text: tweet.text,
+    	in_reply_to_screen_name: tweet.in_reply_to_screen_name,
+    	user: {
+    		name: tweet.user.name,
+    		screen_name: tweet.user.screen_name,
+    		profile_image_url: tweet.user.profile_image_url
+    	},
+    	entities: tweet.entities,
+    	timestamp_ms: tweet.timestamp_ms
+    });
 }, "Failed to insert tweet into TweetCache collection.");
 
 Meteor.methods({
 	streamStatuses: function(data) {
 		var client = twitterCredentials(Meteor.user());
 		if(!data) {
-			data = {
-				track: 'javascript'
-			}
+			throw Meteor.Error( 500, "'null' is not a valid stream query");
 		}
 
 		client.stream('statuses/filter', data, function(stream) {
 			twtQueries[data.track] = stream;
 		  stream.on('data', function(tweet) {
-		  	wrappedTweetInsert(tweet);
+		  	wrappedTweetInsert(tweet, data._id);
 		  });
 
 		  stream.on('error', function(error) {
-		    throw error;
+			throw Meteor.Error( 500, "An error has occured while trying to stream");
 		  });
 		});
 	},
