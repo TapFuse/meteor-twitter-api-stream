858bd99bde4c3682810a27ea0c0984023a40b8a7
Pauli B <pauli.buccini@gmail.com>
2016-10-13 20:46:22 +0700

    added wait for connection limit

--- package.js
+++ package.js
@@ -1,9 +1,14 @@
 Package.describe({
   name: 'tapfuse:twitter-api-streaming',
-  version: '2.3.0',
+  version: '4.0.0',
   summary: 'Tweet caching',
   git: '',
   documentation: 'README.md'
+});
+
+Npm.depends({
+  'he': '1.1.0',
+  'twitter': '1.4.0',
 });
 
 var S = 'server';
@@ -11,27 +16,25 @@ var C = 'client';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.0.1');
-  api.use('ecmascript');
-  api.use('accounts-base');
-  api.use('accounts-password@1.1.11');
-  api.use('accounts-twitter');
-  api.use('mongo');
-  api.use('oauth1');
-  api.use('matb33:collection-hooks@0.8.1');
-  //Dependency
-  api.use('tapfuse:collection-global@2.0.0');
-  api.use('tapfuse:twitter-api@1.0.0');
-  api.use('fongandrew:find-and-modify@0.2.2');
-  //Files
-  api.addFiles('lib/collection-server-tp_tweetCache.js', S);
-  api.addFiles('lib/collection-server-tp_tweetQueries.js', S);
-  api.addFiles('lib/collection-client.js', C);
-  api.addFiles('twitter-api-streaming.js', S);
-});
-
-Npm.depends({
-  "he": "0.5.0"
+    api.versionsFrom('1.2.1');
+    // Core
+    api.use([
+      'ecmascript',
+      'mongo',
+      'promise',
+      'accounts-base',
+      'accounts-password@1.1.11',
+      'accounts-twitter',
+      'oauth1'
+    ]);
+    // 3rd party
+    api.use([
+      'tapfuse:collection-global@2.0.0',
+      'matb33:collection-hooks@0.8.1'
+    ]);
+    api.mainModule('lib/collection-server-tp_tweetCache.js', S);
+    api.mainModule('lib/collection-server-tp_tweetQueries.js', S);
+    api.mainModule('lib/twitter-api-streaming.js', S);
 });
 
 Package.onTest(function(api) {
