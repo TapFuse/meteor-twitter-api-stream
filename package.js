6c5d538a6c7f3d21ec31edea63d1675bbef09adb
Po Buchi <po@ox2.io>
2017-02-05 17:18:09 +0700

    updated to support bearer token

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'tapfuse:twitter-api-streaming',
-  version: '4.2.0',
+  version: '6.0.0',
   summary: 'Tweet caching',
   git: '',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  'he': '1.1.0',
-  'twitter': '1.4.0',
+  'he': '1.1.1',
+  'twitter': '1.7.0',
 });
 
 var S = 'server';
@@ -16,21 +16,21 @@ var C = 'client';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.2.1');
+    api.versionsFrom('1.4.1');
     // Core
     api.use([
       'ecmascript',
       'mongo',
       'promise',
       'accounts-base',
-      'accounts-password@1.1.11',
+      'accounts-password@1.3.3',
       'accounts-twitter',
       'oauth1'
     ]);
     // 3rd party
     api.use([
       'tapfuse:collection-global@2.0.0',
-      'matb33:collection-hooks@0.8.1'
+      'matb33:collection-hooks@0.8.4'
     ]);
     api.mainModule('lib/collection-server-tp_tweetCache.js', S);
     api.mainModule('lib/collection-server-tp_tweetQueries.js', S);
